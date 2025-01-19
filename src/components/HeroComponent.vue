857ce4ce2defb34a944f61514d724df37286540a
Iryna <irynazankovska@gmail.com>
2025-01-18 21:47:42 -0500

    init commit

---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,151 @@
+<template>
+  <section class="hero__container hero">
+    <h1 class="hero__title">
+      The easiest and fastest way to <span>Invest</span> in Agriculture
+    </h1>
+    <p class="hero__paragraph paragraph">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu, mollis
+      aenean sit dictum tincidunt. Ut arcu, suscipit ac etLorem ipsum dolor sit
+      amet.Lorem ipsum dolor sit amet, consectetur
+    </p>
+    <div class="hero__form">
+      <input
+        class="input"
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Enter your email address"
+      />
+      <button class="button button--green">Get started</button>
+    </div>
+    <div class="hero__logos">
+      <img src="../assets/images/MarketWatch Logo.svg" alt="MarketWatch Logo" />
+      <img src="../assets/images/Forbes.com Logo.svg" alt="Forbes.com Logo" />
+      <img
+        src="../assets/images/Business Insider Logo.svg"
+        alt="Business Insider Logo"
+      />
+      <img
+        src="../assets/images/Fidelity Management and Research Company Logo.svg"
+        alt="Fidelity Management and Research Company Logo"
+      />
+      <img src="../assets/images/WIRED UK Logo.svg" alt="WIRED UK Logo" />
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@use "../assets/styles/common-styles/container.scss" as *;
+.hero {
+  padding: 4rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  font-family: $fontFamily;
+  width: 100%;
+  height: 100%;
+  background: url("../assets/images/Star.svg") no-repeat;
+  background-position: 80% 80%;
+  @media screen and (max-width: $tablet) {
+    gap: 1rem;
+    padding: 4rem 1rem;
+  }
+
+  @media screen and (max-width: $mobile) {
+    gap: 0.8rem;
+    padding: 4rem 0;
+  }
+  // .hero__container
+
+  &__container {
+  }
+
+  // .hero__title
+
+  &__title {
+    font-size: 70px;
+    font-weight: 700;
+    text-align: center;
+    padding: 5rem 0;
+    max-width: 900px;
+    @media screen and (max-width: $tablet) {
+      font-size: 50px;
+      padding: 3rem 0;
+    }
+
+    @media screen and (max-width: 670px) {
+      font-size: 40px;
+      padding: 1rem 0;
+    }
+  }
+
+  // .hero__paragraph
+
+  &__paragraph {
+    text-align: center;
+    max-width: 725px;
+  }
+
+  // .hero__form
+
+  &__form {
+    display: flex;
+    flex-direction: row;
+    padding: 30px;
+    max-width: 900px;
+    @media screen and (max-width: 500px) {
+      padding: 0;
+      flex-direction: column;
+      width: 100%;
+    }
+  }
+  &__logos {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 20px;
+    width: 100%;
+    flex-wrap: wrap;
+    padding: 10px;
+    @media screen and (max-width: $tablet) {
+      //
+    }
+    @media screen and (max-width: 702px) {
+      justify-content: space-around;
+    }
+    @media screen and (max-width: $mobile) {
+      padding: 0;
+    }
+    img {
+      width: 150px;
+      @media screen and (max-width: $tablet) {
+        //width: 15%;
+      }
+      @media screen and (max-width: $mobile) {
+        //width: 25%;
+      }
+    }
+  }
+}
+.input {
+  // .input
+}
+
+.button {
+  // .button--green
+
+  &--green {
+    border-radius: 0 10px 10px 0;
+    @media screen and (max-width: $mobile) {
+      border-radius: 10px;
+    }
+  }
+}
+span {
+  color: $secondaryColor;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="main">
+    <HeroComponentVue />
+  </main>
+</template>
+
+<script setup>
+import HeroComponentVue from "../components/HeroComponent.vue";
+</script>
+<style lang="scss" scoped></style>
